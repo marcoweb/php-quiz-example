83b752d5d436de71cb9f18315b9a1000bd5eb287
marcoweb <marcocarvalho.web@gmail.com>
2021-09-28 16:10:25 +0000

    init

---
+++ quiz.php
@@ -0,0 +1,13 @@
+<?php
+$questions = [
+    'QUESTION' => 'A ETEC é vinculada à qual instituição de educação técnológica?',
+    'ALTERNATIVES' => [
+        'A' => 'Centro Paula Souza',
+        'B' => 'Instituto Federal',
+        'C' => 'Nucleo de Tecnologia do Estado  de São Paulo',
+        'D' => 'Nenhuma das Anteriores'
+    ],
+    'CORRECT' => 'A'
+];
+
+?>
